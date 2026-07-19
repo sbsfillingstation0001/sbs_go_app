4ab4c3e7d2bbc798d54debf4b19917f12d174426
sbsfillingstation0001 <sbsfillingstation0001@gmail.com>
2026-07-20 00:35:47 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+// Service Worker for PWA
+self.addEventListener('install', (event) => {
+    console.log('Service Worker installed');
+});
+
+self.addEventListener('fetch', (event) => {
+    // Background fetch handling
+});
